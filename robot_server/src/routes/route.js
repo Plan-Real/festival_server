9cf8cc8c1a2cb2d05e5f3d76159b8c454570a70e
ujma1234 <ujma1234@naver.com>
2023-05-13 22:09:05 +0900

    라우터 - 페이지 연동

--- robot_server/src/routes/route.js
+++ robot_server/src/routes/route.js
@@ -5,6 +5,7 @@ const Controller = require('../controller/controller.js');
 
 router.get('/page/*', Controller.Page)
 
+// router.get('/take_photo/*',Controller.Camera)
 // router.get('/print', Controller.Print)
 
 module.exports = router
