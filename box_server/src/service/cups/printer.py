73dc6d1e5442f7aa68985e2afe8eef4dc35153ea
ujma1234 <ujma1234@naver.com>
2023-06-01 23:14:03 +0900

    remote cups

--- box_server/src/service/cups/printer.py
+++ box_server/src/service/cups/printer.py
@@ -13,7 +13,6 @@ photo_path = os.path.join((os.path.dirname(__file__)), "../../../view/assets/pho
 frame_path = os.path.join((os.path.dirname(__file__)), f"../../../view/assets/frame/{args.decimal}L.png")
 
 img = cv2.imread(photo_path, cv2.IMREAD_COLOR)
-img = img[40:1040,220:1700,:]
 frame = cv2.imread(frame_path, cv2.IMREAD_COLOR)
 frame = cv2.resize(frame, (1480, 1000))
 
@@ -21,6 +20,8 @@ output_path = os.path.join((os.path.dirname(__file__)), "merge.jpg")
 mask_path = os.path.join((os.path.dirname(__file__)), f"../../../view/assets/frame/{args.decimal}_mask.png")
 
 mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+mask = cv2.resize(mask, (1480, 1000))
+img = cv2.resize(img, (1480,1000))
 
 out = cv2.copyTo(img, mask, frame)
 cv2.imwrite(output_path, out)
@@ -30,16 +31,12 @@ import os
 
 conn = cups.Connection()
 printers = list(conn.getPrinters().keys())
-printer_name = printers[0]
-printer_info = conn.getPrinterAttributes(printer_name)
-# media_max = printer_info['media']
-
-path = os.path.join(os.path.dirname(__file__), "merge.jpg")
-
+printer_name = printers[-1]
+# path = os.path.join(os.path.dirname(__file__), "../../../view/assets/photo/merge.jpg")
 conn.printFile(printer_name,
                output_path,"",{
-                'media-bottom-margin-supported': '0',
-                'media-top-margin-supported': '0',
-                'media-left-margin-supported': '0',
-                'media-right-margin-supported': '0',
+                   "media-bottom-margin-supported" : "0",
+                   "media-top-margin-supported" : "0",
+                   "media-left-margin-supported" : "0",
+                   "media-right-margin-supported" : "0"
                }) 
